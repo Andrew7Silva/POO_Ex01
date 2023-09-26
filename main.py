1d67dc4dc875b916134dc77567d3c723d44c4299
Andrew7Silva <reginaldoandrew07@gmail.com>
2023-09-26 16:05:46 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from alunoDeGraduacao import alunoDeGraduacao, alunoDePosGraduacao
 
-grad_aluno = alunoDeGraduacao(nome= '', matricula= '')
-grad_aluno.setNome('Andrew')
-grad_aluno.setMatricula('1234')
+grad_aluno = alunoDeGraduacao(nome="", matricula="")
+grad_aluno.setNome("Andrew")
+grad_aluno.setMatricula("1234")
 print(grad_aluno)
 
-gradPosAluno = alunoDePosGraduacao(nome='' , matricula='', orientador='')
-gradPosAluno.setNome('Andrew Jr')
-gradPosAluno.setMatricula('12345')
-gradPosAluno.setOrientador('Fábio Bezerra')
+gradPosAluno = alunoDePosGraduacao(nome="", matricula="", orientador="")
+gradPosAluno.setNome("Andrew Jr")
+gradPosAluno.setMatricula("12345")
+gradPosAluno.setOrientador("Fábio Bezerra")
 print(gradPosAluno)
-
-test = alunoDeGraduacao(nome='', matricula='')
-test.setNome('Teste da Silva')
-test.setMatricula('testando 123...')
-print(test)
